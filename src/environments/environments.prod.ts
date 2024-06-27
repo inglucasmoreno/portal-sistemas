dbc539eab29aaef25d4ecc4886695f4e394715dd
Moreno Lucas Omar <ing.lucasmoreno@gmail.com>
2024-06-26 23:11:15 -0300

    Para produccion

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,5 +1,5 @@
 export const environments = {
   production: true,
-  // base_url: 'http://10.15.8.10:3031/api'
-  base_url: 'http://45.178.3.232:3035/api'
+  base_url: 'http://10.15.8.10:3031/api'
+  // base_url: 'http://45.178.3.232:3035/api'
 }
